26425078760239b8969b56fc46d3506999582add
rdm <riccardodimaio11@gmail.com>
2020-05-16 12:46:42 +0200

    Refactor main loop

--- eliza.py
+++ eliza.py
@@ -2,7 +2,7 @@ import os
 
 from utils.startup import setup
 from utils.rules import reset_all_last_used_reassembly_rule
-from utils.response import generate_response
+from utils.response import prepare_response, generate_response
 
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 SCRIPT_DIR = os.path.join(PROJECT_DIR, 'scripts')
@@ -25,15 +25,12 @@ def main():
         # if a string contains any characters of the alphabet.
         # Source: https://stackoverflow.com/a/59301031
         if not in_str_l.islower():
-            in_str = input('Eliza: Please, use letters. I am human, after all.\nYou:')
-            continue
-
-        if in_str_l == 'reset':
+            response = prepare_response('Eliza: Please, use letters. I am human, after all.')
+        elif in_str_l == 'reset':
             reset_all_last_used_reassembly_rule(script)
-            in_str = input('Eliza: Reset complete.\nYou:')
-            continue
-
-        response = generate_response(in_str, script, general_script['substitutions'], memory_stack, memory_inputs)
+            response = prepare_response('Eliza: Reset complete.')
+        else:
+            response = generate_response(in_str, script, general_script['substitutions'], memory_stack, memory_inputs)
 
         # Get next user input
         in_str = input(response)
